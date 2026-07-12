86ddbc06260f07d8b41efabca3a644ac1f5432ab
tahashieenavaz <tahashieenavaz@gmail.com>
2026-07-12 12:02:35 +0200

    command_line: adds read and edit for documents in mcp server

--- projects/command_line/mcp_server.py
+++ projects/command_line/mcp_server.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -12,9 +13,42 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+
+@mcp.tool(
+    name="read_document_content",
+    description="Reads the contents of a document and returns it as a String.",
+)
+def read_document_content(
+    document_identifier: str = Field(
+        description="The identifier of a document to read"
+    ),
+) -> str:
+    if document_identifier not in docs:
+        raise ValueError(f"Document {document_identifier} does not exist.")
+
+    return docs[document_identifier]
+
+
+@mcp.tool(
+    name="edit_document",
+    description="Edits the contents of a document and raises a ValueError if the document has not been founded.",
+)
+def edit_document(
+    document_identifier: str = Field(
+        description="The identifier of the document to edit."
+    ),
+    new_content: str = Field(
+        description="The new content to be swapped with the old content of the document."
+    ),
+) -> None:
+    if document_identifier not in docs:
+        raise ValueError(f"Document {document_identifier} was not founded.")
+
+    docs[document_identifier] = new_content
+
+
 # TODO: Write a resource to return all doc id's
+
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
